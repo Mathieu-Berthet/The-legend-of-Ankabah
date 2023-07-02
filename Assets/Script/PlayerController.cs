3be68360efd264b0e0a06e4fd856fab45726580d
Jarmen kell <walczak.adrien.pro@gmail.com>
2023-07-02 18:20:41 +0200

    Update: Jump /Double Jump

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,9 +1,11 @@
 
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
+    // this sctript is for the movement of the player
     [SerializeField] // see variable on inspector
     private float speed = 3f;
 
@@ -12,11 +14,21 @@ public class PlayerController : MonoBehaviour
             [SerializeField] // see variable on inspector
     private float mouseSensitivityY = 3f;
 
+    [SerializeField]
+    private float jumpHeight = 5f;
+
+    [SerializeField]
+    private int maxJumps = 2;
+    private int jumpsRemaining;
+
+    private bool isJumping = false;
+
     private PlayerMotor motor;
 
     private void Start()
     {
         motor = GetComponent<PlayerMotor>(); // link PalyerMotor on this class
+        jumpsRemaining = maxJumps;
     }
 
     private void Update()
@@ -31,7 +43,11 @@ public class PlayerController : MonoBehaviour
         Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
 
         motor.Move(velocity);
-
+                // Detecter l'input de saut
+        if (Input.GetButtonDown("Jump"))
+        {
+            StartJump();
+        }
         // update rotation of the player with a vector3
         float yRotation = Input.GetAxisRaw("Mouse X");
 
@@ -46,4 +62,46 @@ public class PlayerController : MonoBehaviour
 
         motor.RotateCamera(cameraRotation);
     }
+
+    private void StartJump()
+    {
+         if (jumpsRemaining > 0)
+        {
+            jumpsRemaining--;
+
+           if (!isJumping)
+            {
+                isJumping = true;
+
+                // Calculer la vélocité du saut
+                float jumpVelocity = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics.gravity.y));
+
+                // Ajouter la vélocité du saut au mouvement vertical
+                Vector3 jumpVelocityVector = Vector3.up * jumpVelocity;
+                motor.ApplyJump(jumpVelocityVector);
+
+                // Démarrer une coroutine pour réinitialiser l'état de saut après un certain délai
+                StartCoroutine(ResetJump());
+            }
+        }
+
+    }
+    
+    private IEnumerator ResetJump()
+    {
+        yield return new WaitForSeconds(0.1f); // Délai avant de réinitialiser l'état de saut
+        isJumping = false;
+    }
+private void OnCollisionEnter(Collision collision)
+{
+    if (collision.gameObject.CompareTag("Ground"))
+    {
+        ResetJumps();
+    }
+}
+    public void ResetJumps()
+{
+    jumpsRemaining = maxJumps;
+}
+
 }
